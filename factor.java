72696f7854decf31b4469a4dbf381c2dfb0e0556
as26uk <ajz@live.co.uk>
2021-10-31 18:19:16 +0000

    Initial commit

---
+++ factor.java
@@ -0,0 +1,15 @@
+public static void main(String []args) {
+        //System.out.println("Hello World"); // prints Hello World
+
+
+        int x = 10;
+        int f = 10;
+        while (1 < x) {
+        System.out.println(x + " " + f);
+        x = x - 1;
+        f = f * x;
+
+
+        }
+        }
+        }
